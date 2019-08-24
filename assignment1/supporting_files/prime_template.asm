00815b4815dff2398076ce8c96204691df375c66
akshatkarani <akshatkarani@gmail.com>
2019-08-24 11:18:52 +0530

    prime_template.asm: Completed

--- assignment1/supporting_files/prime_template.asm
+++ assignment1/supporting_files/prime_template.asm
@@ -1,8 +1,22 @@
 	.data
 a:
-	10
+	4
 	.text
-// your code here to check if the number 'a' is prime. If yes, write 1 to x10. Else write -1.
-// you may change the value of 'a'
-// remove these comments!
-
+main:
+	load %x0, $a, %x2
+	addi %x0, 2, %x3
+	subi %x2, 1, %x4
+	beq %x2, %x3, prime
+loop:
+	div %x2, %x3, %x5
+	mul %x5, %x3, %x5
+	beq %x2, %x5, notprime
+	addi %x3, 1, %x3
+	bgt %x3, %x4, prime
+	jmp loop
+notprime:
+	subi %x0, 1, %x10
+	end
+prime:
+	addi %x0, 1, %x10
+	end
