133049e72859b7d643af40e0a0905e4f93cbb176
akshatkarani <akshatkarani@gmail.com>
2019-08-24 11:35:52 +0530

    fibonacci_template.asm: Completed

--- assignment1/supporting_files/fibonancci_template.asm
+++ assignment1/supporting_files/fibonancci_template.asm
@@ -1,7 +1,20 @@
 	.data
 n:
-	10
+	12
 	.text
-// your code here to get the first 'n' Fibonacci numbers
-// you may change the value of 'n'
-// remove these comments!
+main:
+	addi %x0, 0, %x2
+	addi %x0, 1, %x3
+	load %x0, $n, %x4
+	addi %x0, 65535, %x5
+loop:
+	store %x2, 0, %x5
+	subi %x4, 1, %x4
+	subi %x5, 1, %x5
+	beq %x4, %x0, endl
+	add %x2, %x0, %x6
+	add %x3, %x0, %x2
+	add %x6, %x3, %x3
+	jmp loop
+endl:
+	end
